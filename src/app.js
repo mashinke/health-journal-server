1a873cfa3e107f6d3bb58ea7868f4e8509fedb9b
Masha Trius <triusmaria@gmail.com>
2020-12-28 13:32:30 -0600

    initialize Express app

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+
+const app = express();
+
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test',
+}));
+app.use(cors());
+app.use(helmet());
+
+module.exports = app;
