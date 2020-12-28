0108e1596f43fdde2fb362f03b0e6f95b4288398
Masha Trius <triusmaria@gmail.com>
2020-12-28 14:41:27 -0600

    implement user creation

---
+++ src/user/user-router.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const path = require('path');
+const UserService = require('./user-service');
+
+const userRouter = express.Router();
+const jsonBodyParser = express.json();
+
+userRouter
+  .post('/', jsonBodyParser, async (req, res, next) => {
+    const { password, username, email } = req.body;
+
+    for (const field of ['email', 'username', 'password'])
+      if (!req.body[field])
+        return res.status(400).json({
+          error: `Missing '${field}' in request body`
+        });
+
+    try {
+      const passwordError = UserService.validatePassword(password);
+
+      if (passwordError)
+        return res.status(400).json({ error: passwordError });
+
+      const hasUserWithEmail = await UserService.hasUserWithEmail(
+        req.app.get('db'),
+        email
+      );
+
+      if (hasUserWithEmail)
+        return res.status(400).json({ error: 'Email already taken' });
+
+      const invalidEmail = !UserService.validateEmail(email);
+
+      if (invalidEmail)
+        return res.status(400).json({ error: 'Invalid email address' });
+
+      const hashedPassword = await UserService.hashPassword(password);
+
+      const newUser = {
+        username,
+        password: hashedPassword,
+        email,
+      };
+
+      const user = await UserService.insertUser(
+        req.app.get('db'),
+        newUser
+      );
+
+      res
+        .status(201)
+        .location(path.posix.join(req.originalUrl, `/${user.id}`))
+        .json(UserService.serializeUser(user));
+    } catch (error) {
+      next(error);
+    }
+  });
+
+module.exports = userRouter;
