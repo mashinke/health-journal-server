b7371536e37562d06944c7cb4be841b9d62165af
Masha Trius <triusmaria@gmail.com>
2020-12-29 12:10:36 -0600

    add a migration with record-related tables

---
+++ migrations/002.do.create_record_tables.sql
@@ -0,0 +1,29 @@
+BEGIN;
+
+CREATE TABLE "form" (
+  "id" SERIAL PRIMARY KEY,
+  "name" TEXT NOT NULL,
+  "description" TEXT,
+  "body" JSONB NOT NULL,
+  "id_user" INTEGER REFERENCES "user"(id) NOT NULL
+);
+
+CREATE TABLE "record" (
+  "id" SERIAL PRIMARY KEY,
+  "created" TIMESTAMP DEFAULT NOW(),
+  "body" JSONB NOT NULL,
+  "id_form" INTEGER REFERENCES "form"(id) NOT NULL
+);
+
+CREATE TABLE "tag" (
+  "id" SERIAL PRIMARY KEY,
+  "name" TEXT
+);
+
+CREATE TABLE "record_tag" (
+  "id" SERIAL PRIMARY KEY,
+  "id_tag" INTEGER REFERENCES "tag"(id),
+  "id_record" INTEGER REFERENCES "record"(id)
+);
+
+COMMIT;
---
+++ migrations/002.undo.create_record_tables.sql
@@ -0,0 +1,5 @@
+BEGIN;
+
+DROP TABLE "form", "record", "tag", "record_tag";
+
+COMMIT;
